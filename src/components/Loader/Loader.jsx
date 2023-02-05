9fb404e47efb8ef6297b2164b9e9f90201983653
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-05 18:47:39 +0100

    Create Loader.jsx

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import { Rings } from "react-loader-spinner";
+
+import styles from "./Loader.module.css";
+
+const Loader = () => {
+    return (
+        <div className={styles.Loader}>
+            <Rings height="100" width="100" color="#576849" arialLabel="loading" />
+        </div>
+    );
+};
+
+export default Loader;
