0d261e0d62d2d8a93c57bb374378ad01b2bd8ff8
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-09 11:31:24 +0100

    Create fetchImages.js

---
+++ src/API/fetchImages.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const fetchImages = async (query, page) => {
+    const BASE_URL = `https://pixabay.com/api/`;
+    const KEY = `31060909-4e678ec3d147c1701bb88f00c`;
+    const options = `image_type=photo&orientation=horizontal&per_page=12`;
+
+    try {
+        const response = await axios.get(
+            `${BASE_URL}?q=${query}&page=${page}&key=${KEY}&${options}`
+        );
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
